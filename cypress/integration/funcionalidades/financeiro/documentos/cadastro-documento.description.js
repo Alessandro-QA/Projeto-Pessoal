fdfc30e0d0842f988196e866870be1b75727f888
Guilherme Gomes da Silva <guilherme.silva@aliare.co>
2022-06-17 09:56:16 -0300

    Criar Seed de teste e adequar Spec

---
+++ cypress/integration/funcionalidades/financeiro/documentos/cadastro-documento.description.js
@@ -0,0 +1,126 @@
+const testDescription = {
+    testes1: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+  
+    ### Cenário 1: Cadastrar Documento dedutível 
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar que o documento é dedutível
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+     ---
+
+    ### Cenário 2: Cadastrar Documento com tag
+    >**DADO**  que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar uma tag
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+     ---
+    
+    ### Cenário 3: Cadastrar Documento com observação
+    >**DADO** que eu queira incluir um novo documento
+    **QUANDO** eu preencher todos os campos obrigatórios
+     **E** preencher o campo de Observação
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+     
+    ### Cenário 4: Cadastrar Documento já pago
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar que o documento já foi pago
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `,
+    testes2: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+
+    ### Cenário 1: Cadastrar Documento já pago e com rateio entre ciclos
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar que o documento já foi pago
+     **E** selecionar o Rateio entre os ciclos
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    
+    ### Cenário 2: Cadastrar Documento com rateio entre ciclos, com duas ou mais categoria
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar o rateio entre duas ou mais categorias
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos 
+    
+    ### Cenário 3: Cadastrar Documento não dedutível
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** que eu queira incluir um novo documento
+     **E** selecionar que o documento não é dedutível
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `,
+
+    testes3: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+
+    ### Cenário 1: Cadastrar Documento parcelado, com rateio entre ciclos
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar Parcelado
+     **E** selecionar Rateio entre os ciclos
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `,
+
+    testes4: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+
+    ### Cenário 1: Cadastrar Documento com rateio de categoria
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar uma ou mais categorias
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `,
+
+    testes5: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+
+    ### Cenário 1: Cadastrar Documento com rateio entre ciclos, com duas ou mais categorias
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** selecionar rateio entre os ciclos
+     **E** selecionar o rateio entre duas ou mais categorias
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `,
+
+    testes6: `  ---
+    ### Funcionalidade: Cadastro de Documento
+    >**COMO** gestor da Fazenda
+     **QUERO** registrar cupons fiscais, notas de despesas outros documentos.
+     **PARA** melhor gerenciar os gastos e controle de documentos da empresa.
+
+    ### Cenário 1: Cadastrar Documento com um anexo
+    >**DADO** que eu queira incluir um novo documento
+     **QUANDO** eu preencher todos os campos obrigatórios
+     **E** anexar um arquivo no documento
+     **E** clicar em Adicionar Documento
+     **ENTÃO** o registro será gravado, exibido e validado na dashboard de Documentos
+    `
+}
+
+export default testDescription
--- cypress/integration/funcionalidades/financeiro/documentos/cadastro-documento.spec.js
+++ cypress/integration/funcionalidades/financeiro/documentos/cadastro-documento.spec.js
@@ -5,7 +5,9 @@ import seedTestDocumento2 from '../../../../fixtures/funcionalidades/financeiro/
 import seedTestDocumento3 from '../../../../fixtures/funcionalidades/financeiro/documentos/cadastro/documento1533.json'
 import seedTestDocumento4 from '../../../../fixtures/funcionalidades/financeiro/documentos/cadastro/documento7553.json'
 import seedTestDocumento5 from '../../../../fixtures/funcionalidades/financeiro/documentos/cadastro/documento3557.json'
+import seedTestDocumento6 from '../../../../fixtures/funcionalidades/financeiro/documentos/cadastro/documento5537.json'
 import Documentos from '../../../../support/commands/funcionalidades/financeiro/documentos/documentos.js'
+import testDescription from './cadastro-documento.description.js'
 
 describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@documentos' }, () => {
   before(function () {
@@ -19,6 +21,7 @@ describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@doc
 
   it('Dedutivel, com tag, observação, já pago, boleto', function () {
     cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes1)
 
     Documentos.cadastrar(seedTestDocumento1)
 
@@ -27,6 +30,7 @@ describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@doc
 
   it('Dedutivel, já pago, dinheiro, com rateio de ciclos e categoria', function () {
     cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes2)
 
     Documentos.cadastrar(seedTestDocumento2.documento)
 
@@ -35,6 +39,7 @@ describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@doc
 
   it('Dedutivel, observação, parcelado, duas parcelas, boleto, com rateio de ciclos', function () {
     cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes3)
 
     Documentos.cadastrar(seedTestDocumento3.documento)
 
@@ -43,6 +48,7 @@ describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@doc
 
   it('Não dedutivel, com tag, cartão de débito, com rateio de categoria', function () {
     cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes4)
 
     Documentos.cadastrar(seedTestDocumento4.documento)
 
@@ -51,9 +57,19 @@ describe('FUNCIONALIDADE > Documentos | Cadastro de Documento - ', { tags: '@doc
 
   it('Não dedutivel, pago, cartão de crédito, com rateio de ciclos e com rateio de categoria', function () {
     cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes5)
 
     Documentos.cadastrar(seedTestDocumento5.documento)
 
     Documentos.validarDetalhes(seedTestDocumento5.detalhes)
+  })
+
+  it('Não dedutivel, transferência bancaria, parcelado, com anexo', function () {
+    cy.allure().severity('critical').startStep('test content')
+    .description(testDescription.testes6)
+
+    Documentos.cadastrar(seedTestDocumento6.documento)
+
+    Documentos.validarDetalhes(seedTestDocumento6.detalhes)
   })
 })
