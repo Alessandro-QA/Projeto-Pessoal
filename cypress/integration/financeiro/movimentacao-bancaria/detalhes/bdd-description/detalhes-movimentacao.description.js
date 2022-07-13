a31619ca1619edd3eba39eb38f80ba43f8e7f9a6
Guilherme Gomes da Silva <guilherme.silva@aliare.co>
2022-07-13 15:34:02 -0300

    Criar seed de test e spec

---
+++ cypress/integration/financeiro/movimentacao-bancaria/detalhes/bdd-description/detalhes-movimentacao.description.js
@@ -0,0 +1,26 @@
+const html = {
+  detalhes: `
+<div>
+    <span style="color: #800080; font-weight: bold;"> Funcionalidade: </span>
+    <span style="color: #b22222;"> Detalhes de Movimentação Bancaria </span>
+</div>
+<div style="margin-left: 40px;">
+  <strong>COMO</strong> gerente/responsável financeiro da fazenda <br/>
+  <strong>QUERO</strong> consultar os detalhes das movimentações bancarias <br/>
+  <strong>PARA</strong> melhor gerenciar as movimentações da minha fazenda <br/>
+</div>
+</br>
+<div>
+  <span style="color: #800080; font-weight: bold;"> Cenario: </span>
+  <span style="color: #b22222;"> Detalhes da movimentação </span>
+</div>
+<div style="margin-left: 40px;">
+  <strong>DADO</strong> que eu queria consultar os detalhes de uma movimentação bancaria realizada <br/>
+  <strong>QUANDO</strong> eu entrar na Listagem de movimentações bancaria <br/>
+  <strong>E</strong> selecionar um card de movimentação <br/>
+  <strong>ENTAO</strong> será apresentado os detalhes da movimentação realizada <br/>
+</div>
+  `,
+}
+
+export default html
---
+++ cypress/integration/financeiro/movimentacao-bancaria/detalhes/detalhes-movimentacao.spec.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+import seedTestDocumento from '../../../../fixtures/funcionalidades/financeiro/movimentaca-bancaria/detalhes/documento.json'
+import seedTestDetalhes from '../../../../fixtures/funcionalidades/financeiro/movimentaca-bancaria/detalhes/detalhes-movimentacao.json'
+import testDescription from './bdd-description/detalhes-movimentacao.description.js'
+import Movimentacao from '../../../../support/commands/funcionalidades/financeiro/movimentacoes-bancarias/movimentacao-bancaria.js'
+import Utils from '../../../../support/utils/utils.js'
+import Authenticate from '../../../../support/commands/funcionalidades/login/login-logout.js'
+
+context('Funcionalidade', () => {
+  describe('Movimentação Bancaria | Detalhes da movimentação bancaria', { tags: '@movimentacaoBancaria' }, () => {
+    var dataAtual = Utils.getDate()
+    var bodyDocumentoPagamento = Utils.replacer('dataSubstituicao', dataAtual, seedTestDocumento.documentoPagamento)
+    var bodyDocumentoRecebimento = Utils.replacer('dataSubstituicao', dataAtual, seedTestDocumento.documentoRecebimento)
+
+    before(function () {
+      const credenciais = Cypress.env('login_cenarios')
+      Authenticate.login(credenciais)
+      Utils.setAccessTokenToEnv(credenciais)
+    })
+
+    after(() => {
+      Authenticate.logout()
+    })
+
+    context('Cadastro do documento ja pago', () =>{
+      it('Via API', function () {
+        Utils.requestApi('POST', '/api/financeiro/v1/Documento', bodyDocumentoPagamento, 'login_cenarios')
+        Utils.requestApi('POST', '/api/financeiro/v1/Documento', bodyDocumentoRecebimento, 'login_cenarios')
+      })
+    })
+
+    context('Validar os detalhes da movimentação bancaria', () => {
+      it('Selecionar movimentacao do tipo pagamento e validar detalhes', function () {
+        cy.allure().severity('normal').startStep('test contet')
+          .descriptionHtml(testDescription.detalhes)
+
+        Movimentacao.detalhes(seedTestDetalhes.detalhesPagamento)
+      })
+
+      it('Selecionar movimentacao do tipo recebimento e validar detalhes', function () {
+        cy.allure().severity('normal').startStep('test contet')
+          .descriptionHtml(testDescription.detalhes)
+       
+        Movimentacao.detalhes(seedTestDetalhes.detalhesRecebimento)
+      })
+    })
+  })
+})
