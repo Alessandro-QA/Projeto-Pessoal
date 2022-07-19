7b9ca7782eee5449c9123e40e2084fa19a00fcc9
Guilherme Gomes da Silva <guilherme.silva@aliare.co>
2022-07-19 11:50:30 -0300

    Ajustando specs

--- cypress/integration/producao/colheita/excluir/excluir.spec.js
+++ cypress/integration/producao/colheita/excluir/excluir.spec.js
@@ -1,69 +1,51 @@
 /// <reference types="cypress" />
 
-import colheitaJson from '../../../../fixtures/cenarios-de-teste/producao/colheita/editar-colheita/cadastro-colheita.json'
+import seedTestColheita from '../../../../fixtures/cenarios-de-teste/producao/colheita/editar-colheita/cadastro-colheita.json'
 import seedTestDashboard from '../../../../fixtures/cenarios-de-teste/producao/colheita/excluir-colheita/dashboard-colheita.json'
-import Colheita from '../../../../support/commands/funcionalidades/producao/colheita.js'
-import Utils from '../../../../support/utils/utils.js'
-import Authenticate from '../../../../support/commands/funcionalidades/login/login-logout.js'
+import testDescription from './bdd-description/excluir.description.js'
+import { excluir, validarListagem, getColheitaPorAmbiente } from '../../../../support/commands/funcionalidades/producao/colheita.js'
+import { getDate, replacer, setAccessTokenToEnv, requestApi } from '../../../../support/utils/utils.js'
+import { login, logout } from '../../../../support/commands/funcionalidades/login/login-logout.js'
 
-context('Funcionalidade', () => {
-  describe('Colheitas | Exclusão de colheita', { tags: '@colheita' }, () => {
-    var dataAtual = Utils.getDate()
-    var bodyColheita = Utils.replacer('dataSubstituicao', dataAtual, colheitaJson)
+// TODO: Bug 41593: Conversão de unidade está divergente entre as bases de Dev, QA e Produção
+// Os teste de cadastro de colheita no Ambiente de QA estão em pausa devido a divergência nos ambiente, onde
+// será necessário aguardar a resolução do bug descrito para a reativalção do mesmo
+if ((Cypress.env('ambiente') === 'dev')) {
+  context('Funcionalidade', () => {
+    describe('Colheitas | Exclusão de colheita', { tags: '@colheita' }, () => {
+      var colheita = getColheitaPorAmbiente(seedTestColheita)
 
-    before(function () {
-      const credenciais = Cypress.env('login_cenarios')
-      Authenticate.login(credenciais)
-      Utils.setAccessTokenToEnv(credenciais)
+      var dataAtual = getDate()
+      var bodyColheita = replacer('dataSubstituicao', dataAtual, colheita)
+
+      before(function () {
+        const credenciais = Cypress.env('login_cenarios')
+        login(credenciais)
+        setAccessTokenToEnv(credenciais)
+      })
+
+      after(() => {
+        logout()
+      })
+
+      it('Cadastrar colheita por API', function () {
+        cy.allure().severity('normal').startStep('test content')
+
+        requestApi('POST', '/api/producao-agricola/v1/colheitas', bodyColheita, 'login_cenarios')
+      })
+
+      it('Excluir colheita', function () {
+        cy.allure().severity('critical').startStep('test content')
+          .descriptionHtml(testDescription.excluir)
+
+        excluir(seedTestDashboard.dashboard)
+      })
+
+      it('Validar exclusão na listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
+        cy.allure().severity('normal').startStep('test content')
+
+        validarListagem(seedTestDashboard.dashboarAposExclusao)
+      })
     })
-
-    after(() => {
-      Authenticate.logout()
-    })
-
-    it('Cadastrar colheita por API', function () {
-      cy.allure().severity('normal').startStep('test content')
-
-      Utils.requestApi('POST', '/api/producao-agricola/v1/colheitas', bodyColheita, 'login_cenarios')
-    })
-
-    it('Excluir colheita', function () {
-      cy.allure().severity('critical').startStep('test content')
-        .descriptionHtml(html)
-
-      Colheita.excluir(seedTestDashboard.dashboard)
-    })
-
-    it('Validar exclusão na listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
-      cy.allure().severity('normal').startStep('test content')
-
-      Colheita.validarListagem(seedTestDashboard.dashboarAposExclusao)
-    })
-
-    const html = `
-<div>
-    <span style="color: #800080; font-weight: bold;"> Funcionalidade: </span>
-    <span style="color: #b22222;"> Excluir colheita </span>
-</div>
-<div style="margin-left: 40px;">
-    <strong>COMO</strong> gestor da Fazenda <br/>
-    <strong>QUERO</strong> poder excluir uma colheita <br/>
-    <strong>PARA</strong> gerenciar quanto colhi durante o ciclo e armazenei em destino interno ou externo <br/>
-</div>
-</br>
-<div>
-    <span style="color: #800080; font-weight: bold;"> Cenario : </span>
-    <span style="color: #b22222;"> Excluir </span>
-</div>
-<div style="margin-left: 40px;">
-    <strong>DADO</strong> que eu queira excluir uma colheita <br/>
-    <strong>QUANDO</strong> eu entrar na dashboard de colheita <br/>
-    <strong>E</strong> clicar no card de uma das colheitas listadas <br/>
-    <strong>E</strong> clicar no ícone de exclusão <br/>
-    <strong>E</strong> clicar em Excluir <br/>
-    <strong>ENTAO</strong> deverá ser salvo e validado na dashboard de colheita <br/>
-</div>
-</div>
-  `
   })
-})
+}
