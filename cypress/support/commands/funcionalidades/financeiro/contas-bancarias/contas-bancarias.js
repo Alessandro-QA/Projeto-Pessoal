af34e3ce0ad5db2013c530d4ed7242cc14a5ceb9
Guilherme Gomes da Silva <guilherme.silva@aliare.co>
2022-08-18 10:27:31 -0300

    Adequar function e locators de cadastro de conta bancaria para cadastro e editar conta bancaria

--- cypress/support/commands/funcionalidades/financeiro/contas-bancarias/contas-bancarias.js
+++ cypress/support/commands/funcionalidades/financeiro/contas-bancarias/contas-bancarias.js
@@ -7,46 +7,97 @@ class ContaBancaria {
    * Metodo para o cadastro de uma conta Bancaria
    * @param {*} seedTestContaBancaria
    */
-  cadastro(seedTestContaBancaria) {
+  cadastroEditar(seedTestContaBancaria) {
     const url = '/financeiro/contas-bancarias'
     const locatorTituloPagina = locContaBancaria.dashboard.titulo
     const tituloPagina = 'Contas bancárias'
 
-    cy.intercept('POST', '/api/financeiro/v1/ContaBancaria').as('contaBancaria')
+    cy.intercept('GET', '/api/financeiro/v1/ContaBancaria/**').as('detalhesConta')
 
     // Navegar para Contas Bancárias
     cy.navegarPara(url, locatorTituloPagina, tituloPagina)
 
-    // botao adicionar conta
-    cy.getVisible(locContaBancaria.dashboard.novaConta).click()
+    // Se for adicionar uma conta nova
+    if (seedTestContaBancaria.adicionar) {
+      // botao adicionar conta
+      cy.getVisible(locContaBancaria.dashboard.novaConta).click()
 
-    cy.getVisible(locContaBancaria.contaBancaria.titulo).should(($el) => {
-      expect($el).to.contain.text('Nova Conta')
-    })
+      cy.getVisible(locContaBancaria.contaBancaria.titulo).should(($el) => {
+        expect($el).to.contain.text('Nova Conta')
+      })
+    } 
+    // Editar uma conta existente
+    else {
+      // input pesquisar
+      cy.getVisible(locContaBancaria.dashboard.pesquisarConta).clear()
+        .type(seedTestContaBancaria.nomeConta)
 
-    // tipo de conta
-    cy.get(locContaBancaria.contaBancaria.tipoConta).click()
-      .contains(seedTestContaBancaria.tipoConta).click()
+      // selecionar a conta bancaria listada
+      if (seedTestContaBancaria.numeroCartao) {
+        // card conta bancaria
+        cy.getVisible(locContaBancaria.dashboard.nomeCartaoCredito)
+          .contains(seedTestContaBancaria.nomeConta).click()
+      } else {
+        // card cartao de credito
+        cy.getVisible(locContaBancaria.dashboard.nomeContaBancaria)
+          .contains(seedTestContaBancaria.nomeConta).click()
+      }
+
+      cy.wait('@detalhesConta')
+
+      // validar nome da conta na tela de detalhes
+      cy.getVisible(locContaBancaria.detalhesConta.nomeConta).should(($el) => {
+        expect($el).to.contain.text(seedTestContaBancaria.nomeConta)
+      })
+
+      // clicar no botão de editar conta bancaria
+      cy.getVisible(locContaBancaria.detalhesConta.buttonEditar).click()
+
+      cy.wait('@detalhesConta')
+    }
+
+    if (seedTestContaBancaria.adicionar) {
+      // tipo de conta
+      cy.get(locContaBancaria.contaBancaria.tipoConta).click()
+        .contains(seedTestContaBancaria.tipoConta).click()
+    } else {
+      // Validar o tipo de conta
+      cy.getVisible(locContaBancaria.contaBancaria.tipoConta).should(($el) => {
+        expect($el).to.contain.text(seedTestContaBancaria.tipoConta)
+      })
+    }
 
     // nome da conta bancaria
     cy.getVisible(locContaBancaria.contaBancaria.nomeConta).clear()
       .type(seedTestContaBancaria.nomeConta)
 
-    // empresa titular
-    cy.get(locContaBancaria.contaBancaria.empresaTitular).click()
-      .contains(seedTestContaBancaria.empresaTitular).click()
+    if (seedTestContaBancaria.adicionar) {
+      // empresa titular
+      cy.getVisible(locContaBancaria.contaBancaria.empresaTitular).click()
+        .contains(seedTestContaBancaria.empresaTitular).click()
+    } else {
+      // validar empresa titular da conta bancária
+      cy.getVisible(locContaBancaria.contaBancaria.empresaTitular).should(($el) => {
+        expect($el).to.contain.text(seedTestContaBancaria.empresaTitular)
+      })
+    }
 
     // empresas habilitadas
-    cy.get(locContaBancaria.contaBancaria.empresasHabilitadas).click()
+    cy.getVisible(locContaBancaria.contaBancaria.empresasHabilitadas).click()
       .contains(seedTestContaBancaria.empresasHabilitadas).click()
 
+    if (seedTestContaBancaria.contaPrincipal) {
+      cy.getVisible(locContaBancaria.contaBancaria.contaPrincipal).click()
+    }
+
+    // Se a conta for do Tipo Conta Corrente/Tesouraria
     if (seedTestContaBancaria.dataSaldoInicial) {
       // data do saldo inicial
-      cy.getVisible(locContaBancaria.contaBancaria.dataSaldoInicial)
+      cy.getVisible(locContaBancaria.contaBancaria.dataSaldoInicial).clear()
         .type(`${seedTestContaBancaria.dataSaldoInicial}{enter}`)
 
       // saldo inicial
-      cy.getVisible(locContaBancaria.contaBancaria.saldoInicial)
+      cy.getVisible(locContaBancaria.contaBancaria.saldoInicial).clear()
         .type(seedTestContaBancaria.saldoInicial)
 
       // valida saldo Atual
@@ -54,73 +105,115 @@ class ContaBancaria {
         expect($el).to.have.value(seedTestContaBancaria.saldoAtual)
       })
 
+      // Se a conta for do tipo Conta Corrente
       if (seedTestContaBancaria.banco) {
         // banco
         cy.get(locContaBancaria.contaBancaria.banco).click()
           .contains(seedTestContaBancaria.banco).click()
 
         // agencia
-        cy.getVisible(locContaBancaria.contaBancaria.agencia)
+        cy.getVisible(locContaBancaria.contaBancaria.agencia).clear()
           .type(seedTestContaBancaria.agencia)
 
         // digito da agencia
-        cy.getVisible(locContaBancaria.contaBancaria.agenciaDigito)
+        cy.getVisible(locContaBancaria.contaBancaria.agenciaDigito).clear()
           .type(seedTestContaBancaria.agenciaDigito)
 
         // numero da conta
-        cy.getVisible(locContaBancaria.contaBancaria.numeroConta)
+        cy.getVisible(locContaBancaria.contaBancaria.numeroConta).clear()
           .type(seedTestContaBancaria.numeroConta)
 
         // digito da conta
-        cy.getVisible(locContaBancaria.contaBancaria.contaDigito)
+        cy.getVisible(locContaBancaria.contaBancaria.contaDigito).clear()
           .type(seedTestContaBancaria.contaDigito)
       }
-    } else {
+    } 
+    // Se a conta for do Tipo cartão de crédito
+    else {
       // valida saldo Atual
       cy.getVisible(locContaBancaria.contaBancaria.saldoAtual).should(($el) => {
         expect($el).to.have.value(seedTestContaBancaria.saldoAtual)
       })
 
-      // seleciona a bandeira do cartão
-      cy.getVisible(locContaBancaria.contaBancaria.selectBandeira).click()
-        .find('li').contains(seedTestContaBancaria.bandeira).click()
+      if (seedTestContaBancaria.adicionar) {
+        // seleciona a bandeira do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.selectBandeira).click()
+          .find('li').contains(seedTestContaBancaria.bandeira).click()
 
-      // seleciona a Data do fechamento
-      cy.getVisible(locContaBancaria.contaBancaria.dataFechamento)
-        .type(seedTestContaBancaria.dataFechamento)
+        // seleciona a Data do fechamento
+        cy.getVisible(locContaBancaria.contaBancaria.dataFechamento).clear()
+          .type(seedTestContaBancaria.dataFechamento)
 
-      // informar a data de vencimento do cartao
-      cy.getVisible(locContaBancaria.contaBancaria.dataVencimento)
-        .type(seedTestContaBancaria.dataVencimento)
+        // informar a data de vencimento do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.dataVencimento).clear()
+          .type(seedTestContaBancaria.dataVencimento)
 
-      // informar o numero do cartão
-      cy.getVisible(locContaBancaria.contaBancaria.numeroCartao)
-        .type(seedTestContaBancaria.numeroCartao)
+        // informar o numero do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.numeroCartao).clear()
+          .type(seedTestContaBancaria.numeroCartao)
 
-      // informar o limite do cartão
-      cy.getVisible(locContaBancaria.contaBancaria.limiteCartao)
-        .type(seedTestContaBancaria.limiteCartao)
+        // informar o limite do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.limiteCartao).clear()
+          .type(seedTestContaBancaria.limiteCartao)
 
-      // selecionar a conta que será vinculada ao cartão
-      cy.getVisible(locContaBancaria.contaBancaria.selectContaVinculada).click()
-        .find('li').contains(seedTestContaBancaria.contaVinculada).click({ force: true })
+        // selecionar a conta que será vinculada ao cartão
+        cy.getVisible(locContaBancaria.contaBancaria.selectContaVinculada).click()
+          .find('li').contains(seedTestContaBancaria.contaVinculada).click({ force: true })
+      } else {
+        // Valida a bandeira do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.selectBandeira).should(($el) => {
+          expect($el).to.contain.text(seedTestContaBancaria.bandeira)
+        })
+
+        // Valida a data de fechamento do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.dataFechamento).should(($el) => {
+          expect($el).to.have.value(seedTestContaBancaria.dataFechamento)
+        })
+
+        // Valida a data de vencimento do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.dataVencimento).should(($el) => {
+          expect($el).to.have.value(seedTestContaBancaria.dataVencimento)
+        })
+
+        // Valida o numero do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.numeroCartao).should(($el) => {
+          expect($el).to.have.value(seedTestContaBancaria.numeroCartao)
+        })
+
+        // Valida o limite do cartão
+        cy.getVisible(locContaBancaria.contaBancaria.limiteCartao).should(($el) => {
+          expect($el).to.have.value(seedTestContaBancaria.limiteCartao)
+        })
+
+        // Valida a conta vinculada ao cartão
+        cy.getVisible(locContaBancaria.contaBancaria.selectContaVinculada).should(($el) => {
+          expect($el).to.contain.text(seedTestContaBancaria.contaVinculada)
+        })
+      }
     }
 
+    // Clica no icone de adicionar o valor do saldo a dashboard financeira
     if (seedTestContaBancaria.incluirSaldo) {
       cy.getVisible(locContaBancaria.contaBancaria.incluirSaldo).click()
     }
 
-    // botao adicionar conta
+    // inativa ou ativa a conta bancaria
+    if (seedTestContaBancaria.ativarInativar) {
+      cy.getVisible(locContaBancaria.contaBancaria.ativarInativar).click()
+    }
+
+    // botão adicionar conta
     cy.getVisible(locContaBancaria.contaBancaria.adicionar)
       .click()
 
-    cy.wait('@contaBancaria')
+    cy.wait('@detalhesConta')
 
+    // valida mensagem de sucesso
     cy.get(locContaBancaria.contaBancaria.mensagemSucesso).should(($el) => {
       expect($el).exist.and.to.contain.text('Conta salva com sucesso')
     })
 
-    // validar que o btão de adicionar não exista mais
+    // validar que o botão de adicionar não exista mais
     cy.get(locContaBancaria.contaBancaria.adicionar).should('not.exist')
   }
 
