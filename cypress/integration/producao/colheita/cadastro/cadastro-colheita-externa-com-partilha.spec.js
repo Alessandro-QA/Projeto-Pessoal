33b2b3ace08cf51c447353e11f3ebebcf8f6a37c
Gabriel Messias Ribeiro dos Santos <gabriel.santos@hubconexa.com>
2023-02-02 18:58:36 -0300

    Ajustando teste de colheita para executar em QA

--- cypress/integration/producao/colheita/cadastro/cadastro-colheita-externa-com-partilha.spec.js
+++ cypress/integration/producao/colheita/cadastro/cadastro-colheita-externa-com-partilha.spec.js
@@ -10,56 +10,50 @@ import { validarDashboard } from '../../../../support/commands/producao/dashboar
 import { getDate, replacer, requestApi, setAccessTokenToEnv, getPayloadPorAmbiente } from '../../../../support/utils/utils.js'
 import { login, logout } from '../../../../support/commands/login/login-logout.js'
 
-// TODO: Bug 41593: Conversão de unidade está divergente entre as bases de Dev, QA e Produção
-// Os teste de cadastro de colheita no Ambiente de QA estão em pausa devido a divergência nos ambiente, onde
-// será necessário aguardar a resolução do bug descrito para a reativalção do mesmo
-if ((Cypress.env('ambiente') === 'dev')) {
-  describe('Produção', { tags: '@producao' }, () => {
-    var contrato = getPayloadPorAmbiente(payloadContrato)
+describe('Produção', { tags: '@producao' }, () => {
 
-    var dataAtual = getDate()
-    var bodyContrato1 = replacer('dataSubstituicao', dataAtual, contrato.contrato1)
-    var bodyContrato2 = replacer('dataSubstituicao', dataAtual, contrato.contrato2)
+  var dataAtual = getDate()
+  var bodyContrato1 = replacer('dataSubstituicao', dataAtual, getPayloadPorAmbiente(payloadContrato).contrato1)
+  var bodyContrato2 = replacer('dataSubstituicao', dataAtual, getPayloadPorAmbiente(payloadContrato).contrato2)
 
-    before(function () {
-      const credenciais = Cypress.env('login_cenarios')
-      login(credenciais)
-      setAccessTokenToEnv(credenciais)
-    })
+  before(function () {
+    const credenciais = Cypress.env('login_cenarios')
+    login(credenciais)
+    setAccessTokenToEnv(credenciais)
+  })
 
-    after(() => {
-      logout()
-    })
-    describe('Colheita', { tags: '@colheita' }, () => {
-      describe('Cadastro', { tags: '@cadastro' }, () => {
-        context('Colheita externa com partilha', () => {
-          it('Deve cadastrar contratos por API', function () {
-            cy.allure().severity('normal').startStep('test content')
+  after(() => {
+    logout()
+  })
+  describe('Colheita', { tags: '@colheita' }, () => {
+    describe('Cadastro', { tags: '@cadastro' }, () => {
+      context('Colheita externa com partilha', () => {
+        it('Deve cadastrar contratos por API', function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato1, 'login_cenarios')
-            requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato2, 'login_cenarios')
-          })
+          requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato1, 'login_cenarios')
+          requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato2, 'login_cenarios')
+        })
 
-          it('Deve cadastrar colheita externa', function () {
-            cy.allure().severity('critical').startStep('test content')
-              .descriptionHtml(testDescription.externaPartilha)
+        it('Deve cadastrar colheita externa', function () {
+          cy.allure().severity('critical').startStep('test content')
+            .descriptionHtml(testDescription.externaPartilha)
 
-            cadastrarEditar(seedTestCadastro)
-          })
+          cadastrarEditar(seedTestCadastro)
+        })
 
-          it('Deve validar listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
-            cy.allure().severity('normal').startStep('test content')
+        it('Deve validar listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            validarListagem(seedTestDashboardColheita)
-          })
+          validarListagem(seedTestDashboardColheita)
+        })
 
-          it('Deve validar dashboard de Produção', function () {
-            cy.allure().severity('normal').startStep('test content')
+        it('Deve validar dashboard de Produção', function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            validarDashboard(seedTestDashboardProducao)
-          })
+          validarDashboard(seedTestDashboardProducao)
         })
       })
     })
   })
-}
+})
--- cypress/integration/producao/colheita/cadastro/cadastro-colheita-externa-excedente.spec.js
+++ cypress/integration/producao/colheita/cadastro/cadastro-colheita-externa-excedente.spec.js
@@ -4,71 +4,63 @@ import seedTestDashboardColheita from '../../../../fixtures/producao/colheita/ex
 import seedTestCadastro from '../../../../fixtures/producao/colheita/externa-com-quantidade-excedente.js/cadastro-colheita.json'
 import seedTestDashboardProducao from '../../../../fixtures/producao/colheita/externa-com-quantidade-excedente.js/dashboard-producao.json'
 import payloadContrato from '../../../../fixtures/producao/colheita/externa-com-quantidade-excedente.js/contrato.json'
-// TODO: aguardando resolução do bug https://dev.azure.com/conexalabs/ProjetoX/_workitems/edit/36869
-// import seedTestDashboardContrato from '../../../../fixtures/producao/colheita/externa-com-quantidade-excedente.js/dashboard-contrato.json'
+import seedTestDashboardContrato from '../../../../fixtures/producao/colheita/externa-com-quantidade-excedente.js/dashboard-contrato.json'
 import testDescription from './bdd-description/cadastro-colheita.description.js'
 import { cadastrarEditar, validarListagem } from '../../../../support/commands/producao/colheita.js'
+import Contrato from '../../../../support/commands/producao/contratos.js'
 import { validarDashboard } from '../../../../support/commands/producao/dashboardProducao.js'
 import { getDate, replacer, requestApi, setAccessTokenToEnv, getPayloadPorAmbiente } from '../../../../support/utils/utils.js'
 import { login, logout } from '../../../../support/commands/login/login-logout.js'
 
-// TODO: Bug 41593: Conversão de unidade está divergente entre as bases de Dev, QA e Produção
-// Os teste de cadastro de colheita no Ambiente de QA estão em pausa devido a divergência nos ambiente, onde
-// será necessário aguardar a resolução do bug descrito para a reativalção do mesmo
-if ((Cypress.env('ambiente') === 'dev')) {
-  describe('Produção', { tags: '@producao' }, () => {
-    var contrato = getPayloadPorAmbiente(payloadContrato)
+describe('Produção', { tags: '@producao' }, () => {
+  var bodyContrato1 = replacer('dataSubstituicao', getDate(), getPayloadPorAmbiente(payloadContrato).contrato1)
+  var bodyContrato2 = replacer('dataSubstituicao', getDate(), getPayloadPorAmbiente(payloadContrato).contrato2)
 
-    var dataAtual = getDate()
-    var bodyContrato1 = replacer('dataSubstituicao', dataAtual, contrato.contrato1)
-    var bodyContrato2 = replacer('dataSubstituicao', dataAtual, contrato.contrato2)
+  before(function () {
+    const credenciais = Cypress.env('login_cenarios')
+    login(credenciais)
+    setAccessTokenToEnv(credenciais)
+  })
 
-    before(function () {
-      const credenciais = Cypress.env('login_cenarios')
-      login(credenciais)
-      setAccessTokenToEnv(credenciais)
-    })
+  after(() => {
+    logout()
+  })
 
-    after(() => {
-      logout()
-    })
-    describe('Colheita', { tags: '@colheita' }, () => {
-      describe('Cadastro', { tags: '@cadastro' }, () => {
-        context('Colheita externa com partilha e quantidade excedente', () => {
-          it('Deve cadastrar contratos por API', function () {
-            cy.allure().severity('normal').startStep('test content')
+  describe('Colheita', { tags: '@colheita' }, () => {
+    describe('Cadastro', { tags: '@cadastro' }, () => {
+      context('Colheita externa com partilha e quantidade excedente', () => {
+        it('Deve cadastrar contratos por API', function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato1, 'login_cenarios')
-            requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato2, 'login_cenarios')
-          })
+          requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato1, 'login_cenarios')
+          requestApi('POST', '/api/producao-agricola/v1F/contratos', bodyContrato2, 'login_cenarios')
+        })
 
-          it('Deve cadastrar colheita externa', function () {
-            cy.allure().severity('critical').startStep('test content')
-              .descriptionHtml(testDescription.externaExcedente)
+        it('Deve cadastrar colheita externa', function () {
+          cy.allure().severity('critical').startStep('test content')
+            .descriptionHtml(testDescription.externaExcedente)
 
-            cadastrarEditar(seedTestCadastro)
-          })
+          cadastrarEditar(seedTestCadastro)
+        })
 
-          it('Deve validar listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
-            cy.allure().severity('normal').startStep('test content')
+        it('Deve validar listagem de Colheita', { retries: { runMode: 1, openMode: 1, }, }, function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            validarListagem(seedTestDashboardColheita)
-          })
+          validarListagem(seedTestDashboardColheita)
+        })
 
-          it('Deve validar dashboard de Produção', function () {
-            cy.allure().severity('normal').startStep('test content')
+        it('Deve validar dashboard de Produção', function () {
+          cy.allure().severity('normal').startStep('test content')
 
-            validarDashboard(seedTestDashboardProducao)
-          })
+          validarDashboard(seedTestDashboardProducao)
+        })
 
-          // TODO: aguardando resolução do bug https://dev.azure.com/conexalabs/ProjetoX/_workitems/edit/36869
-          // it('Validar dashboard Contrato', function () {
-          //   cy.allure().severity('normal').startStep('test content')
+        it('Validar dashboard Contrato', function () {
+          cy.allure().severity('normal').startStep('test content')
 
-          //   validarDashboard(seedTestDashboardContrato)
-          // })
+          Contrato.validarDashboard(seedTestDashboardContrato)
         })
       })
     })
   })
-}
+})
