b0c456b4fd20b3b676bf5173cdd780fc75deae0a
Beatriz Guimaraes <beatriz.guimaraes@aliare.co>
2024-06-04 16:36:11 -0300

    Adicionando Teste de Documento por ID

--- api/cypress/e2e/financeiro/documento/documento.cy.js
+++ api/cypress/e2e/financeiro/documento/documento.cy.js
@@ -2,9 +2,15 @@
 
 context('Financeiro', () => {
     context('Documento', () => {
-        describe('', () => {
+        describe('GET - /api/financeiro/v1/Documento - Obtém registros de documentos', () => {
 
-            it('', () => {
+            it('CT1 - Obtém Registro de Documento pelo ID', () => {
+                cy.getRequest('/api/financeiro/v1/Documento/7a58cb86-be72-4881-b0ed-2a04049a9ab6')
+                    .then((response) => {
+                        expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'))
+                        expect(response.status).be.equal(200)
+                        expect(response.body.id).to.equal('7a58cb86-be72-4881-b0ed-2a04049a9ab6');
+                    })
             })
         })
     })
