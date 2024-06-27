727b75224a86f99c6a1802047285198e6405f354
Beatriz Guimaraes <beatriz.guimaraes@aliare.co>
2024-06-27 11:05:24 -0300

    Alterações nos testes

--- e2e/cypress/support/commands/homepage/menus.js
+++ e2e/cypress/support/commands/homepage/menus.js
@@ -9,68 +9,72 @@ class Menus {
     const locatorTituloPagina = locMenus.titulos.titulo
     const tituloPagina = 'Dashboard geral'
 
-    // Navegar para página de dashboard geral (home)
+    // Navega para página de dashboard geral (home)
     cy.navegarPara(url, locatorTituloPagina, tituloPagina)
 
-    // Iterar sobre cada chave (botão) dentro de locators.botoes
+    // Itera sobre cada chave (botão) dentro de locators.botoes
     Object.keys(locMenus.botoes).forEach((buttonKey) => {
       const buttonSelector = locMenus.botoes[buttonKey]
 
-      // Verificar se o botão está presente na página
-      cy.get(buttonSelector).should('be.visible')
+      // Verifica se o botão está presente na página
+      cy.get(buttonSelector)
+        .should('be.visible')
     });
   }
 
   validarAtividadesAgricolas() {
-    // Clicar no botão de Atividades Agrícolas para abrir o sub-menu
+    // Clica no botão de Atividades Agrícolas para abrir o sub-menu
     cy.get(locMenus.botoes.atividadeAgricola).click()
 
-    // Iterar sobre cada chave (botão) dentro de locators.botoesAtividadesAgricolas
-    Object.keys(locMenus.botoesAtividadesAgricolas).forEach((buttonKey) => {
-      const buttonSelector = locMenus.botoesAtividadesAgricolas[buttonKey]
+    // Localiza o elemento pai que contém todos os elementos <a> do submenu
+    cy.get(locMenus.menu.menuAtivo).within(() => {
 
-      // Verificar se o botão está presente na página
-      cy.get(buttonSelector).should('be.visible');
-    });
+      // Pega os textos esperados do arquivo de locators
+      const valoresEsperados = locMenus.textosEsperados.botoesAtividadesAgricolas;
+
+      // Itera sobre cada elemento <a> dentro do submenu e verifica se contém o texto esperado
+      valoresEsperados.forEach(valor => {
+        cy.contains('a', valor)
+          .should('be.visible')
+          .should('have.text', valor); // Verifica se o valor está presente e visível
+      })
+    })
 
     cy.desabilitarPopUpNotificacao()
 
     // Simula o hover ao passar sobre o botão "Painéis" e aguarda até que o menu de opções esteja visível 
-    cy.get(locMenus.menu.menuAtivo).contains('Cadastro')
+    cy.get(locMenus.menu.menuAtivo).contains('Painéis')
       .should('be.visible')
       .trigger('mouseenter')
 
-    // Aguarde até que o elemento <a> dentro do div submenu-holder esteja visível
-    //cy.get('div.submenu-holder a')
-     // .should('be.visible'); // Verifica se o link <a> dentro do div submenu-holder está visível na tela
-
-    // Localize o elemento pai que contém todos os elementos <a> do submenu
+    // Localiza o elemento pai que contém todos os elementos <a> do submenu
     cy.get('div.submenu-holder').within(() => {
-      // Lista dos valores esperados dentro dos elementos <a>
-      const valoresEsperados = [
-        'Ciclo', 'Conta Contábil', 'Conversões de unidade', 'Cultura', 'Detentora',
-        'Empresas', 'Fazendas', 'Material', 'Perfis', 'Pessoas', 'Safra', 'Talhão',
-        'Und. de Armazenamento', 'Und. de Medida', 'Usuários', 'Variedade'
-      ];
+
+      // Pega os textos esperados do arquivo de locators
+      const valoresEsperados = locMenus.textosEsperados.listaPaineis
 
       // Itera sobre cada elemento <a> dentro do submenu e verifica se contém o texto esperado
       valoresEsperados.forEach(valor => {
         cy.contains('a', valor).should('be.visible'); // Verifica se o valor está presente e visível
-      });
-    });
+      })
+    })
 
-      cy.get(locMenus.menu.menuAtivo).within(() => {
-        // Lista dos valores esperados dentro dos elementos <a>
-        const valoresEsperados = [
-          'Dashboard ', 'Planejamento'
-        ];
-  
-        // Itera sobre cada elemento <a> dentro do submenu e verifica se contém o texto esperado
-        valoresEsperados.forEach(valor => {
-          cy.contains(valor).should('be.visible').should('have.text', valor); // Verifica se o valor está presente e visível
-        });
-    });
+    // Simula o hover ao passar sobre o botão "Cadastros" e aguarda até que o menu de opções esteja visível 
+    cy.get(locMenus.menu.menuAtivo).contains('Cadastros')
+      .should('be.visible')
+      .trigger('mouseenter')
 
+    // Localiza o elemento pai que contém todos os elementos <a> do submenu
+    cy.get('div.submenu-holder').within(() => {
+
+      // Pega os textos esperados do arquivo de locators
+      const valoresEsperados = locMenus.textosEsperados.listaCadastros
+
+      // Itera sobre cada elemento <a> dentro do submenu e verifica se contém o texto esperado
+      valoresEsperados.forEach(valor => {
+        cy.contains('a', valor).should('be.visible'); // Verifica se o valor está presente e visível
+      })
+    })
   }
 }
 
--- e2e/cypress/support/locators/homepage/locators-menus.js
+++ e2e/cypress/support/locators/homepage/locators-menus.js
@@ -1,50 +1,38 @@
 const locators = {
-    titulos: {
-      titulo: '.cnx-page-header__container--title',
-    },
-    botoes: {
-      atividadeAgricola: '.siagri-icon-atividades',
-      maquinarios: '.siagri-icon-maquinarios',
-      suprimentos: '[title="Suprimentos"] > .siagri-icon-suprimentos',
-      producao: '.siagri-icon-producao',
-      financeiro: '.siagri-icon-financeiro',
-      resultados: '.siagri-icon-resultados',
-      nfe: '.siagri-icon-nfe'
-    },
-    botoesAtividadesAgricolas: {
-      dashboard: '.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > .swiper-slide-active',
-      planejamento: '.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > .swiper-slide-next',
-      execucao: '.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > :nth-child(3)',
-      clima: '.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > :nth-child(4)',
-      históricoTalhoes:'.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > :nth-child(5)',
-      paineis:'.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > :nth-child(6)',
-      cadastros:'.is-active.navmenu > .hold-children > .swiper-container > .swiper-wrapper > :nth-child(7)',
-    },
-    
-    menu:{
-      menuAtivo: '.is-active.navmenu'
-    }
-    /*botoesMaquinarios: {
+  titulos: {
+    titulo: '.cnx-page-header__container--title',
+  },
+  botoes: {
+    atividadeAgricola: '.siagri-icon-atividades',
+    maquinarios: '.siagri-icon-maquinarios',
+    suprimentos: '[title="Suprimentos"] > .siagri-icon-suprimentos',
+    producao: '.siagri-icon-producao',
+    financeiro: '.siagri-icon-financeiro',
+    resultados: '.siagri-icon-resultados',
+    nfe: '.siagri-icon-nfe',
+  },
 
-    },
-    botoesSuprimentos: {
+  textosEsperados: {
+    botoesAtividadesAgricolas: [
+      'Dashboard ', 'Planejamento ', 'Execução ', 'Clima ', 'Histórico dos talhões ', 'Painéis ', 'Cadastros '
+    ],
 
-    },
-    botoesProducao: {
+    listaPaineis: [
+      'Atividades do Campo'
+    ],
 
-    },
-    botoesFinanceiro: {
+    listaCadastros: [
+      'Ciclo', 'Conta Contábil', 'Conversões de unidade', 'Cultura', 'Detentora',
+      'Empresas', 'Fazendas', 'Material', 'Perfis', 'Pessoas', 'Safra', 'Talhão',
+      'Und. de Armazenamento', 'Und. de Medida', 'Usuários', 'Variedade'
+    ]
+  },
 
-    },
-    botoesResultados: {
-
-    },
-    bototesNFE: {
-      
-    } */
+  menu: {
+    menuAtivo: '.is-active.navmenu'
   }
-  
-  export default locators
-  
-  
-  
+}
+
+export default locators
+
+
