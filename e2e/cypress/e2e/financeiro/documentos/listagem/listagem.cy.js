da666ebe877250a7494206e94abc34da614693a6
Joan Filipe <joan.meira@hubconexa.com>
2024-07-22 09:23:44 -0300

    Feature/98397 - Início

--- e2e/cypress/e2e/financeiro/documentos/listagem/listagem.cy.js
+++ e2e/cypress/e2e/financeiro/documentos/listagem/listagem.cy.js
@@ -4,39 +4,17 @@ import payloadDocumentos from '../../../../fixtures/financeiro/documentos/listag
 import seedTestDocumento from '../../../../fixtures/financeiro/documentos/listagem/listagem.json'
 import { listagem, conferir } from '../../../../support/commands/financeiro/documentos/documentos.js'
 import Utils from '../../../../support/utils/utils.js'
-import testDescription from './bdd-description/listagem.description'
+import testDescription from './bdd-description/listagem.description.js'
 import Authenticate from '../../../../support/commands/login/login-logout.js'
 
 describe('Financeiro', { tags: '@financeiro' }, () => {
-  var dataAtual = Utils.getDate()
+ 
   var documento = Utils.getPayloadPorAmbiente(payloadDocumentos)
-
-  var bodyDocumento789456 = Utils.replacer('dataSubstituicao', dataAtual, documento.documento789456)
-  var bodyDocumento987456 = Utils.replacer('dataSubstituicao', dataAtual, documento.documento987456)
-  var bodyDocumento357357 = Utils.replacer('dataSubstituicao', dataAtual, documento.documento357357)
-
-  before(function () {
-    const credenciais = Cypress.env('login_cenarios')
-    Authenticate.login(credenciais)
-    Utils.setAccessTokenToEnv(credenciais)
-  })
-
-  after(() => {
-    Authenticate.logout()
-  })
 
   describe('Documentos', { tags: '@documentos' }, () => {
     context('Listagem', () => {
       context('Filtragem de Documentos', () => {
-        it('Cadastrar documentos via API', function () {
-          // cy.allure().severity('Critical').startStep('test content')
-
-          Utils.requestApi('POST', '/api/financeiro/v1/Documento', bodyDocumento789456, 'login_cenarios')
-          Utils.requestApi('POST', '/api/financeiro/v1/Documento', bodyDocumento987456, 'login_cenarios')
-          Utils.requestApi('POST', '/api/financeiro/v1/Documento', bodyDocumento357357, 'login_cenarios')
-          Utils.requestApi('POST', '/api/financeiro/v1/Documento', documento.documento753753, 'login_cenarios')
-        })
-
+        
         it('Sem preenchimento de filtro', function () {
           // cy.allure().severity('normal').startStep('test content')
             //.descriptionHtml(testDescription.semFiltro)
