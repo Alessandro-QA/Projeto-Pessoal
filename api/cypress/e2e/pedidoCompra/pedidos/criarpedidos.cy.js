8d594a4d1692cd09700d9197bd14db29011278cc
alessandro.santos <alessandro.santos@hubconexa.com>
2024-07-22 09:04:32 -0300

    Feature/97806

---
+++ api/cypress/e2e/pedidoCompra/pedidos/criarpedidos.cy.js
@@ -0,0 +1,107 @@
+/// <reference types='Cypress' />
+
+const description = require('../../../fixtures/pedidoCompra/pedidos/criarpedidos/criarpedidos.description')
+const { validatePedidos } = require('../../../fixtures/pedidoCompra/pedidos/criarpedidos/validate')
+const dayjs = require('dayjs');
+
+context('Pedido Compra', () => {
+    context('Criar Pedidos', () => {
+
+        let idpedidos
+        let idpedidoPagamentoParcelas
+        let idpedidoPagamento
+        describe(`POST/PUT/PATCH/GET/DELETE - ${Cypress.env('pedidoCompra')}/Pedidos - Criar Pedidos`, () => {
+            it('CT1 - Criação de Pedidos', () => {
+                cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
+
+                cy.fixture('pedidoCompra/pedidos/criarpedidos/payloadCt1.json').then((payload) => {
+                    // Gera um número de pedido aleatório
+                    let randomNumber = Math.floor(Math.random() * 1000000)
+                    payload.numeroPedidoFornecedor = randomNumber // Adiciona o número de pedido ao payload
+
+                    // Obter a data de amanhã no formato desejado (YYYY-MM-DDTHH:mm:ssZ)
+                    const tomorrowDate = dayjs().add(1, 'day').format('YYYY-MM-DDTHH:mm:ssZ');
+                    payload.data = tomorrowDate;
+
+                    cy.postRequest(`${Cypress.env('baseUrl')}${Cypress.env('pedidoCompra')}/Pedidos`, payload)
+                        .then((response) => {
+                            // Verifica o status code da resposta
+                            expect(response.status).to.equal(200)
+                            // Armazenar o ID do pedido criado
+                            idpedidos = response.body.data.id
+                            idpedidoPagamento = response.body.data.pedidoPagamento.id
+                            idpedidoPagamentoParcelas = response.body.data.pedidoPagamento.pedidoPagamentoParcelas[0].id
+
+                            validatePedidos(response.body.data)
+                        })
+                })
+            })
+
+            it('CT2 - Editar de Pedidos', () => {
+                cy.allureDescriptionHtml(description.Ct2).allureSeverity('normal')
+
+                cy.fixture('pedidoCompra/pedidos/criarpedidos/payloadCt2.json').then((payload) => {
+
+                    payload.pedidoPagamento.pedidoPagamentoParcelas[0].id = idpedidoPagamentoParcelas
+                    payload.pedidoPagamento.id = idpedidoPagamento
+
+                    payload.id = idpedidos
+                    let randomNumber = Math.floor(Math.random() * 1000000)
+                    payload.numeroPedidoFornecedor = randomNumber // Adiciona o número de pedido ao payload
+
+                    // Obter a data de amanhã no formato desejado (YYYY-MM-DDTHH:mm:ssZ)
+                    const tomorrowDate = dayjs().add(1, 'day').format('YYYY-MM-DDTHH:mm:ssZ');
+                    payload.data = tomorrowDate;
+
+                    cy.putRequest(`${Cypress.env('baseUrl')}${Cypress.env('pedidoCompra')}/Pedidos`, payload).then((response) => {
+                        // Verifica o status code da resposta
+                        expect(response.status).to.equal(200)
+
+                        validatePedidos(response.body.data)
+                    })
+                })
+            })
+
+            it('CT3 - Obter Pedidos', () => {
+                cy.allureDescriptionHtml(description.Ct3).allureSeverity('normal');
+
+                cy.getRequest(`${Cypress.env('baseUrl')}${Cypress.env('pedidoCompra')}/Pedidos/${idpedidos}`)
+                    .then((response) => {
+                        expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'));
+                        expect(response.status).to.equal(200);
+                        expect(response.body).to.exist;
+                        expect(response.body).to.not.be.null;
+
+                        validatePedidos(response.body);
+                    });
+            });
+
+            it('CT4 - Deve patchear um Pedido', () => {
+                cy.allureDescriptionHtml(description.Ct4).allureSeverity('normal')
+
+                cy.fixture('pedidoCompra/pedidos/criarpedidos/payloadCt3.json').then((payload) => {
+
+                    cy.patchRequest(`${Cypress.env('baseUrl')}${Cypress.env('pedidoCompra')}/Pedidos/${idpedidos}`, payload)
+                        .then((response) => {
+                            // Verifica o status code da resposta
+                            expect(response.status).to.equal(200);
+
+                        });
+                });
+            });
+
+            it('CT5 - Excluir Pedidos', () => {
+                cy.allureDescriptionHtml(description.Ct5).allureSeverity('normal')
+
+                cy.deleteRequest(`${Cypress.env('baseUrl')}${Cypress.env('pedidoCompra')}/Pedidos`, idpedidos).then((response) => {
+                    // Verifica o status code da resposta
+                    expect(response.status).to.equal(200)
+                    // Verifica o corpo da resposta
+                    expect(response.body).to.have.property('success', true)
+                    expect(response.body).to.have.property('data', true)
+                })
+            })
+
+        })
+    })
+})
---
+++ api/cypress/fixtures/pedidoCompra/pedidos/criarpedidos/criarpedidos.description.js
@@ -0,0 +1,80 @@
+const html = {
+    Ct1: `
+      <div>
+        <span style="font-weight: bold; color: black;">Funcionalidade da API:</span>
+        <span style="color: black;">A rota "POST - Pedidos" cria um novo pedido no sistema.</span>
+      </div>
+      <br/>
+      <div>
+        <span style="font-weight: bold; color: black;">Descrição do Teste - CT1:</span>
+        <span style="color: black;">
+          Este teste realiza uma requisição 'POST' para criar um novo pedido, utilizando um payload com um número de pedido gerado aleatoriamente.
+          Verifica se a API retorna um status '200' e armazena o ID do pedido criado, o ID do pagamento do pedido e o ID das parcelas de pagamento do pedido.
+          Além disso, valida os dados do pedido criado.
+        </span>
+      </div>
+    `,
+
+    Ct2: `
+      <div>
+        <span style="font-weight: bold; color: black;">Funcionalidade da API:</span>
+        <span style="color: black;">A rota "PUT - Pedidos" edita um pedido existente no sistema.</span>
+      </div>
+      <br/>
+      <div>
+        <span style="font-weight: bold; color: black;">Descrição do Teste - CT2:</span>
+        <span style="color: black;">
+          Este teste realiza uma requisição 'PUT' para editar um pedido existente, utilizando um payload com os IDs de pagamento e parcelas de pagamento.
+          Adiciona um número de pedido gerado aleatoriamente e verifica se a API retorna um status '200'.
+          Além disso, valida os dados do pedido editado.
+        </span>
+      </div>
+    `,
+
+    Ct3: `
+      <div>
+        <span style="font-weight: bold; color: black;">Funcionalidade da API:</span>
+        <span style="color: black;">A rota "GET - Pedidos/{id}" obtém os detalhes de um pedido específico.</span>
+      </div>
+      <br/>
+      <div>
+        <span style="font-weight: bold; color: black;">Descrição do Teste - CT3:</span>
+        <span style="color: black;">
+          Este teste realiza uma requisição 'GET' para obter os detalhes de um pedido específico utilizando o ID armazenado anteriormente.
+          Verifica se a API retorna um status '200' e se o corpo da resposta não está vazio.
+        </span>
+      </div>
+    `,
+
+    Ct4: `
+      <div>
+        <span style="font-weight: bold; color: black;">Funcionalidade da API:</span>
+        <span style="color: black;">A rota "PATCH - Pedidos/{id}" atualiza parcialmente um pedido específico.</span>
+      </div>
+      <br/>
+      <div>
+        <span style="font-weight: bold; color: black;">Descrição do Teste - CT4:</span>
+        <span style="color: black;">
+          Este teste realiza uma requisição 'PATCH' para atualizar parcialmente um pedido específico utilizando um payload.
+          Verifica se a API retorna um status '200'.
+        </span>
+      </div>
+    `,
+
+    Ct5: `
+      <div>
+        <span style="font-weight: bold; color: black;">Funcionalidade da API:</span>
+        <span style="color: black;">A rota "DELETE - Pedidos" exclui um pedido do sistema.</span>
+      </div>
+      <br/>
+      <div>
+        <span style="font-weight: bold; color: black;">Descrição do Teste - CT5:</span>
+        <span style="color: black;">
+          Este teste realiza uma requisição 'DELETE' para excluir um pedido utilizando o ID armazenado anteriormente.
+          Verifica se a API retorna um status '200' e se o corpo da resposta contém as propriedades 'success' e 'data' com valor 'true'.
+        </span>
+      </div>
+    `,
+};
+
+export default html;
