241192c50db9342565d8383df1eb8fe22f4060ab
Joan Filipe <joan.meira@hubconexa.com>
2024-07-23 10:41:52 -0300

    Feature/98397

--- e2e/cypress/e2e/financeiro/documentos/listagem/listagem.cy.js
+++ e2e/cypress/e2e/financeiro/documentos/listagem/listagem.cy.js
@@ -5,79 +5,93 @@ import seedTestDocumento from '../../../../fixtures/financeiro/documentos/listag
 import { listagem, conferir } from '../../../../support/commands/financeiro/documentos/documentos.js'
 import Utils from '../../../../support/utils/utils.js'
 import testDescription from './bdd-description/listagem.description.js'
-import Authenticate from '../../../../support/commands/login/login-logout.js'
+
+const dayjs = require('dayjs');
 
 describe('Financeiro', { tags: '@financeiro' }, () => {
- 
+
   var documento = Utils.getPayloadPorAmbiente(payloadDocumentos)
 
   describe('Documentos', { tags: '@documentos' }, () => {
     context('Listagem', () => {
       context('Filtragem de Documentos', () => {
-        
+
         it('Sem preenchimento de filtro', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.semFiltro)
+       
+          cy.allureDescriptionHtml(testDescription.semFiltro).allureSeverity('normal')
 
           listagem(seedTestDocumento.semFiltro)
         })
 
         it('Filtrar por Pessoa', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarPessoa)
+         
+          cy.allureDescriptionHtml(testDescription.filtrarPessoa).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroPessoa)
         })
 
         it('Filtrar por Tag', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarTag)
 
+          cy.allureDescriptionHtml(testDescription.filtrarTag).allureSeverity('normal')
+          
           listagem(seedTestDocumento.filtroTag)
         })
 
         it('Filtrar por Conferido', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarConferido)
-
-          conferir(seedTestDocumento.filtroConferido.conferir)
+         
+          cy.allureDescriptionHtml(testDescription.filtrarConferido).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroConferido)
         })
 
         it('Filtrar por Não Conferido', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarConferido)
+          
+          cy.allureDescriptionHtml(testDescription.filtrarConferido).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroNaoConferido)
         })
 
         it('Filtrar por Safra e Ciclo', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarSafraCiclo)
+          
+          cy.allureDescriptionHtml(testDescription.filtrarSafraCiclo).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroSafraCiclo)
         })
 
         it('Filtrar por Fazenda', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarFazenda)
+          
+          cy.allureDescriptionHtml(testDescription.filtrarFazenda).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroFazenda)
         })
 
         it('Filtrar por Data', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarData)
+        
+          cy.allureDescriptionHtml(testDescription.filtrarData).allureSeverity('normal')
 
+          // Data de hoje
+          const hoje = dayjs().format('DD/MM/YYYY'); // Ou qualquer formato que desejar
+
+          // Data de uma semana atrás
+          const umaSemanaAtras = dayjs().subtract(1, 'week').format('DD/MM/YYYY');
+
+          seedTestDocumento.filtroData.dataInicio = umaSemanaAtras
+          seedTestDocumento.filtroData.dataFinal = hoje
           listagem(seedTestDocumento.filtroData)
         })
 
         it('Filtrar por Safra', function () {
-          // cy.allure().severity('normal').startStep('test content')
-            //.descriptionHtml(testDescription.filtrarSafra)
+          
+          cy.allureDescriptionHtml(testDescription.filtrarSafra).allureSeverity('normal')
 
           listagem(seedTestDocumento.filtroSafra)
+        })
+
+        it('Validar quando nenhum documento encontrado', function () {
+          
+          cy.allureDescriptionHtml(testDescription.nenhumEncontrado).allureSeverity('normal')
+
+          listagem(seedTestDocumento.semDocumento)
         })
       })
     })
