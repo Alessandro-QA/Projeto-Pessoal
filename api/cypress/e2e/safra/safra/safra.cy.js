de35a171e30a252a4c86cc635b8efb2d88c08564
Joan Filipe <joan.meira@hubconexa.com>
2024-07-25 14:59:13 -0300

    Manutenção

--- api/cypress/e2e/safra/safra/safra.cy.js
+++ api/cypress/e2e/safra/safra/safra.cy.js
@@ -1,12 +1,12 @@
 /// <reference types='Cypress' />
 
-const description = require('../../../fixtures/safra/safra/safra.description');
+const description = require('../../../fixtures/safra/safra/safra.description')
 
 context('Safra', () => {
     context('Cadastro de Safra', () => {
 
-        let safraID;
-        let randomNumber;
+        let safraID
+        let randomNumber
 
         describe(`POST/PUT/DELETE - ${Cypress.env('safra')}/Safra - Criar/Editar/Apagar Safra`, () => {
 
@@ -17,30 +17,30 @@ context('Safra', () => {
                 cy.fixture('safra/safra/payloadCt1.json').then((payload) => {
 
                     // Gerar um novo número aleatório, pois o documento não pode possuir o mesmo número
-                    randomNumber = Math.floor(Math.random() * 1000000); // Gera um número aleatório entre 0 e 999999
-                    payload.descricao = "Teste Edição " + randomNumber.toString(); // Atualiza o campo 'numero' no payload
+                    randomNumber = Math.floor(Math.random() * 1000000) // Gera um número aleatório entre 0 e 999999
+                    payload.descricao = "Teste Edição " + randomNumber.toString() // Atualiza o campo 'numero' no payload
 
                     cy.postRequest(`${Cypress.env('baseUrlDaas')}${Cypress.env('safra')}/Safra`, payload)
                         .then((response) => {
-                            expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'));
-                            expect(response.status).to.be.equal(200);
-                            expect(response.body).to.not.be.null;
-                            expect(response.body).to.exist;
+                            expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'))
+                            expect(response.status).to.be.equal(200)
+                            expect(response.body).to.not.be.null
+                            expect(response.body).to.exist
 
                             // Verificando o campo data
-                            expect(response.body).to.have.property('data').that.is.an('object');
+                            expect(response.body).to.have.property('data').that.is.an('object')
                             // Verificando os campos dentro de data
-                            expect(response.body.data).to.have.property('id').that.is.a('string');
-                            expect(response.body.data).to.have.property('descricao').that.is.a('string').to.be.equal(payload.descricao);
-                            expect(response.body.data).to.have.property('dataInicial').that.is.a('string').to.be.equal(payload.dataInicial);
-                            expect(response.body.data).to.have.property('dataFinal').that.is.a('string').to.be.equal(payload.dataFinal);
-                            expect(response.body.data).to.have.property('ativo').to.be.true;
+                            expect(response.body.data).to.have.property('id').that.is.a('string')
+                            expect(response.body.data).to.have.property('descricao').that.is.a('string').to.be.equal(payload.descricao)
+                            expect(response.body.data).to.have.property('dataInicial').that.is.a('string').to.be.equal(payload.dataInicial)
+                            expect(response.body.data).to.have.property('dataFinal').that.is.a('string').to.be.equal(payload.dataFinal)
+                            expect(response.body.data).to.have.property('ativo').to.be.true
 
                             safraID = response.body.data.id
 
-                        });
-                });
-            });
+                        })
+                })
+            })
 
             it('CT2 - Deve Editar a Safra Criada', () => {
                 cy.fixture('safra/safra/payloadCt2.json').then((payload) => {
@@ -51,31 +51,31 @@ context('Safra', () => {
 
                     cy.putRequest(`${Cypress.env('baseUrlDaas')}${Cypress.env('safra')}/Safra`, payload)
                         .then((response) => {
-                            expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'));
-                            expect(response.status).to.be.equal(200);
-                            expect(response.body).to.not.be.null;
-                            expect(response.body).to.exist;
+                            expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'))
+                            expect(response.status).to.be.equal(200)
+                            expect(response.body).to.not.be.null
+                            expect(response.body).to.exist
 
                             // Verificando o campo data
-                            expect(response.body).to.have.property('data').that.is.an('object');
+                            expect(response.body).to.have.property('data').that.is.an('object')
                             // Verificando os campos dentro de data
-                            expect(response.body.data).to.have.property('id').that.is.a('string');
-                            expect(response.body.data).to.have.property('descricao').that.is.a('string').to.be.equal(payload.descricao);
-                            expect(response.body.data).to.have.property('dataInicial').that.is.a('string').to.be.equal(payload.dataInicial);
-                            expect(response.body.data).to.have.property('dataFinal').that.is.a('string').to.be.equal(payload.dataFinal);
-                            expect(response.body.data).to.have.property('ativo').to.be.false;
+                            expect(response.body.data).to.have.property('id').that.is.a('string')
+                            expect(response.body.data).to.have.property('descricao').that.is.a('string').to.be.equal(payload.descricao)
+                            expect(response.body.data).to.have.property('dataInicial').that.is.a('string').to.be.equal(payload.dataInicial)
+                            expect(response.body.data).to.have.property('dataFinal').that.is.a('string').to.be.equal(payload.dataFinal)
+                            expect(response.body.data).to.have.property('ativo').to.be.false
 
-                        });
-                });
-            });
+                        })
+                })
+            })
 
             it('CT3 - Deve Deletar a Safra Criada', () => {
                 cy.allureDescriptionHtml(description.Ct3).allureSeverity('normal')
                 cy.deleteRequest(`${Cypress.env('baseUrlDaas')}${Cypress.env('safra')}/Safra`, safraID).then((response) => {
-                    expect(response.status).to.be.equal(200);
-                });
-            });
+                    expect(response.status).to.be.equal(200)
+                })
+            })
 
-        });
-    });
-});
+        })
+    })
+})
