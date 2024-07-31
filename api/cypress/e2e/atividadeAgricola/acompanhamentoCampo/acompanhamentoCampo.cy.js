bc00dbb5d2a4e7c50bb6fd9836c23f72c00384ca
Joan Filipe <joan.meira@hubconexa.com>
2024-07-31 14:57:13 -0300

    Feature/99258 - Complete

--- api/cypress/e2e/atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo.cy.js
+++ api/cypress/e2e/atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo.cy.js
@@ -1,14 +1,15 @@
 /// <reference types='Cypress' />
 
-//const description = require('../../../fixtures/controleClimatico/previsaoClimatica/list/list.description');
+const description = require('../../../fixtures/atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo/acompanhamentoCampo.description')
 import { validateAcompanhamento } from '../../../fixtures/atividadeAgricola/acompanhamentoCampo/validation'
+const dayjs = require('dayjs')
 
 context('Atividade Agrícola', () => {
     context('Acompanhamento de Campo', () => {
         describe(`GET/PUT - ${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo - Ver Detalhes e Atualizar Dados de Acompanhamento do Talhão`, () => {
             it('CT1 - Recuperar dados de Acompanhamento de Campo de Talhão Específico', () => {
 
-                //cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
+                cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
                 
                 cy.fixture('atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo/payloadCt1.json').then((payload) => {
                     cy.getRequest(`${Cypress.env('baseUrlBackoffice')}${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo/${payload.id}`)
@@ -26,17 +27,28 @@ context('Atividade Agrícola', () => {
 
             it('CT2 - Atualizar Dados Talhão Expecífico', () => {
 
-                //cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
+                cy.allureDescriptionHtml(description.Ct2).allureSeverity('normal')
                 
-                cy.fixture('atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo/payloadCt1.json').then((payload) => {
-                    cy.getRequest(`${Cypress.env('baseUrlBackoffice')}${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo/${payload.id}`)
+                cy.fixture('atividadeAgricola/acompanhamentoCampo/acompanhamentoCampo/payloadCt2.json').then((payload) => {
+
+                    payload.contagemPrevistaStand = Math.floor(Math.random() * 101)
+                    payload.contagemRealizadaStand = Math.floor(Math.random() * 101)
+                    payload.espacamento = Math.floor(Math.random() * 101)
+
+                    const value = Math.random() * 1000
+                    payload.pms = Math.round(value * 1000) / 1000
+
+                    payload.dataEmergencia = dayjs().add(5, 'day').format('YYYY-MM-DDTHH:mm:ss[+00:00]')
+                    payload.dataFimPlantio = dayjs().add(30, 'day').format('YYYY-MM-DDTHH:mm:ss[+00:00]')
+
+                    cy.putRequest(`${Cypress.env('baseUrlBackoffice')}${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo`,payload)
                         .then((response) => {
                             expect(response.requestHeaders).to.have.property('x-tenant').to.be.equal(Cypress.env('tenant'))
                             expect(response.status).to.be.equal(200)
                             expect(response.body).to.exist
                             expect(response.body).to.not.be.null
 
-                            //validateAcompanhamento(response.body.data)
+                            validateAcompanhamento(response.body.data)
 
                         })
                 })
--- api/cypress/e2e/atividadeAgricola/acompanhamentoCampo/get.cy.js
+++ api/cypress/e2e/atividadeAgricola/acompanhamentoCampo/get.cy.js
@@ -1,6 +1,6 @@
 /// <reference types='Cypress' />
 
-//const description = require('../../../fixtures/controleClimatico/previsaoClimatica/list/list.description');
+const description = require('../../../fixtures/atividadeAgricola/acompanhamentoCampo/get/get.description')
 import { validateAcompanhamento } from '../../../fixtures/atividadeAgricola/acompanhamentoCampo/validation'
 
 context('Atividade Agrícola', () => {
@@ -8,7 +8,7 @@ context('Atividade Agrícola', () => {
         describe(`POST - ${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo/Get - Obtém a Lista de Talhões Por Fazenda`, () => {
             it('CT1 - Obter Lista filtrada por Fazenda/Safra/Ciclo (Filtros Obrigatórios)', () => {
 
-                //cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
+                cy.allureDescriptionHtml(description.Ct1).allureSeverity('normal')
                 
                 cy.fixture('atividadeAgricola/acompanhamentoCampo/get/payloadCt1.json').then((payload) => {
                     cy.postRequest(`${Cypress.env('baseUrlBackoffice')}${Cypress.env('atividadeAgricola')}/AcompanhamentoCampo/Get`, payload)
