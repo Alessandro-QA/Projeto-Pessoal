0a05b2954315fba4b84ac5e2f13d35a9c5721b67
Beatriz Guimaraes <beatriz.guimaraes@aliare.co>
2024-08-01 15:11:48 -0300

    Ajustando description

--- e2e/cypress/e2e/atividades-agricolas/cadastros/cadastro-pessoa.cy.js
+++ e2e/cypress/e2e/atividades-agricolas/cadastros/cadastro-pessoa.cy.js
@@ -2,19 +2,19 @@
 
 const seedTestPessoa = require('../../../fixtures/atividades-agricolas/cadastros/fixture-pessoa.json')
 const Pessoa = require('../../../support/commands/atividades-agricolas/cadastros/pessoa/pessoa.js')
-//const testDescription = require('./bdd-description/pessoa.description.js')
+const testDescription = require('./bdd-description/pessoa.description.js')
 
 describe('Atividades Agrícolas', { tags: '@atividadesAgricolas' }, () => {
   describe('Cadastros', { tags: '@cadastro' }, () => {
     context('Pessoa', () => {
-      it('CT1 - Deve cadastrar uma pessoa', function () {
-        //cy.allureDescriptionHtml(testDescription.Ct1).allureSeverity('normal')
+      it('CT1 - Deve Cadastrar uma Pessoa Jurídica', function () {
+        cy.allureDescriptionHtml(testDescription.Ct1).allureSeverity('normal')
 
         Pessoa.cadastro(seedTestPessoa)
       })
 
-      it('CT2 - Deve validar o cadastro no Dashboard de Cadastro de pessoas', function () {
-        //cy.allureDescriptionHtml(testDescription.Ct1).allureSeverity('normal')
+      it('CT2 - Deve Validar o Cadastro no Dashboard de Cadastro de Pessoas', function () {
+        cy.allureDescriptionHtml(testDescription.Ct2).allureSeverity('normal')
 
         Pessoa.validarDashboard(seedTestPessoa)
       })
